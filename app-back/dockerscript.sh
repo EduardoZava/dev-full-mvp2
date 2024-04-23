c3efa62eabe961c674a99d59264d64d4a5fcc688
EduardoZava <eduardozava@gamil.com>
2024-04-23 07:59:35 -0300

    ajustando Dockerfile

--- app-back/dockerscript.sh
+++ app-back/dockerscript.sh
@@ -3,8 +3,6 @@ cd temp
 git clone https://github.com/EduardoZava/dev-full-mvp2.git
 cd dev-full-mvp2/app-back
 sudo docker build -t app-back .
-sudo docker stop app-back
-sudo docker rm -f app-back
 sudo docker run -d -p 4000:4000 app-back  
 cd ../../..          
 rm -rf temp
