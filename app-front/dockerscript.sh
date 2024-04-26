fb41b30bca95c97a36845ff9c660565bba4a8397
Jose Eduardo Zava Coelho <jose.coelho@neoris.com>
2024-04-26 11:04:55 -0300

    ajustando erros  docker do front

--- app-front/dockerscript.sh
+++ app-front/dockerscript.sh
@@ -10,8 +10,8 @@ mkdir temp
 cd temp
 git clone https://github.com/EduardoZava/dev-full-mvp2.git
 cd dev-full-mvp2/app-front
-sudo docker build -t app-front .
-sudo docker run -d -p 8080:8080 app-front
+docker build -t app-front .
+docker run -d -p 8080:8080 app-front
 #destroy the temp folder
 cd ../../..          
 rm -rf temp
