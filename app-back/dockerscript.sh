0a64f2d1adac689fc7fba164db9d0b8206adf433
Jose Eduardo Zava Coelho <jose.coelho@neoris.com>
2024-04-26 14:07:12 -0300

    ajustando dockerscript  do back para adiciona a network ente os containers

--- app-back/dockerscript.sh
+++ app-back/dockerscript.sh
@@ -6,6 +6,12 @@ cd temp
 git clone https://github.com/EduardoZava/dev-full-mvp2.git
 cd dev-full-mvp2/app-back
 docker build -t app-back .
-docker run -d -p 4000:4000 app-back  
+if docker network inspect my-network >/dev/null 2>&1; then
+    echo "Network 'my-network' already exists"
+else
+    echo "Network 'my-network' does not exist creating network"
+    docker network create my-network
+fi
+docker run -d --network my-network -p 4000:4000 app-back  
 cd ../../..          
 rm -rf temp
